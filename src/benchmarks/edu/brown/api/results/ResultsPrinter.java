1c21e6dd88cfc505a3a43a9fcd3f95a7c1dbad23
Anurag Khandelwal <anuragk@berkeley.edu>
2016-07-17 17:51:56 -0700

    Switched from ms to ns

--- src/benchmarks/edu/brown/api/results/ResultsPrinter.java
+++ src/benchmarks/edu/brown/api/results/ResultsPrinter.java
@@ -23,14 +23,6 @@
 
 package edu.brown.api.results;
 
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.Map;
-
-import org.apache.log4j.Logger;
-import org.voltdb.utils.Pair;
-
 import edu.brown.api.BenchmarkInterest;
 import edu.brown.hstore.conf.HStoreConf;
 import edu.brown.statistics.Histogram;
@@ -38,6 +30,13 @@ import edu.brown.statistics.HistogramUtil;
 import edu.brown.utils.CollectionUtil;
 import edu.brown.utils.StringUtil;
 import edu.brown.utils.TableUtil;
+import org.apache.log4j.Logger;
+import org.voltdb.utils.Pair;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 /**
  * Standard printer for displaying benchmark results
@@ -53,7 +52,7 @@ public class ResultsPrinter implements BenchmarkInterest {
         "THROUGHPUT",
         "AVG LATENCY",
     };
-    
+
     private static final String COL_FORMATS[] = {
         "%22s:",
         "%8d (%5.1f%%)",
@@ -61,34 +60,34 @@ public class ResultsPrinter implements BenchmarkInterest {
         "%8.2f txn/s",
         "%8s ms",
     };
-    
+
 
     public static final int[] percentiles = { 50, 95, 99 };
-    
+
     private static final String RESULT_FORMAT = "%.2f";
     private static final String SPACER = "  ";
-    
+
     private final HStoreConf hstore_conf;
     private boolean stop = false;
-    
+
     public ResultsPrinter(HStoreConf hstore_conf) {
         this.hstore_conf = hstore_conf;
     }
-    
+
     public void stop() {
         this.stop = true;
     };
-    
+
     @Override
     public String formatFinalResults(BenchmarkResults results) {
         if (this.stop) return (null);
-        
+
         StringBuilder sb = new StringBuilder();
         FinalResult fr = new FinalResult(results);
-        
-        final int width = 100; 
+
+        final int width = 100;
         sb.append(String.format("\n%s\n", StringUtil.header("BENCHMARK RESULTS", "=", width)));
-        
+
         // -------------------------------
         // GLOBAL TOTALS
         // -------------------------------
@@ -101,9 +100,9 @@ public class ResultsPrinter implements BenchmarkInterest {
              .append(" / ")
              .append(String.format("stdev:" + RESULT_FORMAT, fr.getStandardDeviationTxnPerSecond()))
              .append("]");
-        
+
         StringBuilder latencies = new StringBuilder();
-        latencies.append(String.format(RESULT_FORMAT + " ms", fr.getTotalAvgLatency()))
+        latencies.append(String.format(RESULT_FORMAT + " ns", fr.getTotalAvgLatency()))
              .append(" [")
              .append(String.format("min:" + RESULT_FORMAT, fr.getTotalMinLatency()))
              .append(" / ")
@@ -111,19 +110,19 @@ public class ResultsPrinter implements BenchmarkInterest {
              .append(" / ")
              .append(String.format("stdev:" + RESULT_FORMAT, fr.getTotalStdDevLatency()))
              .append("]");
-        
+
         String txnInfo = String.format("Total:%d / Distributed:%d (%.1f%%) / SpecExec:%d (%.1f%%)",
                                         fr.getTotalTxnCount(),
                                         fr.getTotalDtxnCount(),
                                         (fr.getTotalDtxnCount() / (double)fr.getTotalTxnCount())*100,
                                         fr.getTotalSpecExecCount(),
                                         (fr.getTotalSpecExecCount() / (double)fr.getTotalTxnCount())*100);
-        
-        
+
+
         Map<String, Object> m = new LinkedHashMap<String, Object>();
         m.put("Execution Time", String.format("%d ms", fr.getDuration()));
         m.put("Transactions", txnInfo);
-        m.put("Throughput", throughput.toString()); 
+        m.put("Throughput", throughput.toString());
         m.put("Latency", latencies.toString());
         sb.append(StringUtil.formatMaps(m));
         sb.append("\n");
@@ -136,7 +135,7 @@ public class ResultsPrinter implements BenchmarkInterest {
         int num_rows = txnNames.size() + (hstore_conf.client.output_clients ? clientNames.size() + 1 : 0) + 1; // HEADER
         Object rows[][] = new String[num_rows][COL_FORMATS.length];
         int row_idx = 0;
-        
+
         rows[row_idx++] = COL_HEADERS;
         for (String txnName : txnNames) {
             EntityResult er = fr.getTransactionResult(txnName);
@@ -153,18 +152,18 @@ public class ResultsPrinter implements BenchmarkInterest {
                 rows[row_idx][i] = "";
             } // FOR
             row_idx++;
-            
+
             for (String clientName : CollectionUtil.sort(clientNames)) {
                 EntityResult er = fr.getClientResult(clientName);
                 assert(er != null);
                 this.makeRow(er, clientName, rows[row_idx++]);
             } // FOR
         }
-        
+
         sb.append(StringUtil.repeat("-", width)).append("\n");
         sb.append(TableUtil.table(rows));
         sb.append(String.format("\n%s\n", StringUtil.repeat("=", width)));
-        
+
         // -------------------------------
         // TXNS PER PARTITION
         // -------------------------------
@@ -180,7 +179,7 @@ public class ResultsPrinter implements BenchmarkInterest {
             sb.append(StringUtil.prefix(h.toString((int)(width * 0.5)), "   "));
             sb.append(String.format("\n%s\n", StringUtil.repeat("=", width)));
         }
-        
+
         // -------------------------------
         // CLIENT RESPONSES
         // -------------------------------
@@ -191,28 +190,28 @@ public class ResultsPrinter implements BenchmarkInterest {
             sb.append(StringUtil.prefix(h.toString((int)(width * 0.5)), "   "));
             sb.append(String.format("\n%s\n", StringUtil.repeat("=", width)));
         }
-        
+
         return (sb.toString());
     }
-    
+
     private void makeRow(EntityResult er, String label, Object row[]) {
-        
+
         int col_idx = 0;
         row[col_idx++] = String.format(COL_FORMATS[col_idx-1], label);
-        
+
         // TXN COUNT + PERCENTAGE
         row[col_idx++] = String.format(COL_FORMATS[col_idx-1],
                                                  er.getTxnCount(),
                                                  er.getTxnPercentage());
-        
+
         // SINGLE-PARTITON COUNT + PERCENTAGE
         row[col_idx++] = String.format(COL_FORMATS[col_idx-1],
                                                  er.getDistributedTxnCount(),
                                                  er.getDistributedTxnPercentage());
-        
+
         // TXN / MS
         row[col_idx++] = String.format(COL_FORMATS[col_idx-1], er.getTxnPerMilli());
-        
+
         // AVG LATENCY
         String txnAvgLatency = "-";
         if (er.getTxnCount() > 0) {
@@ -220,24 +219,24 @@ public class ResultsPrinter implements BenchmarkInterest {
         }
         row[col_idx++] = String.format(COL_FORMATS[col_idx-1], txnAvgLatency);
     }
-    
+
     @Override
     public void benchmarkHasUpdated(BenchmarkResults results) {
         if (this.stop) return;
         if (hstore_conf.client.output_interval == false) return;
-        
+
         Pair<Long, Long> p = results.computeTotalAndDelta();
         assert(p != null);
         long totalTxnCount = p.getFirst();
         long txnDelta = p.getSecond();
-        
+
         // INTERVAL LATENCY
         Histogram<Integer> lastLatencies = results.getLastTotalLatencies();
         double intervalLatency = HistogramUtil.sum(lastLatencies) / (double)lastLatencies.getSampleCount();
 //        double[] intervalPerLatency = HistogramUtil.percentile(lastLatencies, percentiles);
-        
+
         // TOTAL LATENCY
-        Histogram<Integer> allLatencies = results.getAllTotalLatencies();        
+        Histogram<Integer> allLatencies = results.getAllTotalLatencies();
         double totalLatency = HistogramUtil.sum(allLatencies) / (double)allLatencies.getSampleCount();
 
         int pollIndex = results.getCompletedIntervalCount();
@@ -254,18 +253,18 @@ public class ResultsPrinter implements BenchmarkInterest {
         sb.append("\n" + SPACER + SPACER);
         sb.append(String.format("Completed %d txns at a rate of " + RESULT_FORMAT + " txns/s",
                                 txnDelta, txnDelta / (double)(results.getIntervalDuration()) * 1000d));
-        sb.append(String.format(" with " + RESULT_FORMAT + " ms avg latency", intervalLatency));        
+        sb.append(String.format(" with " + RESULT_FORMAT + " ms avg latency", intervalLatency));
 //        sb.append("\n" + SPACER);
-//        sb.append(String.format(" Percentile Latencies 50: " + RESULT_FORMAT + "(ms) Latencies 95: " 
-//                    + RESULT_FORMAT + "(ms) Latencies 99: " + RESULT_FORMAT + "(ms)", intervalPerLatency[0],intervalPerLatency[1], intervalPerLatency[2]));        
-        
-        sb.append("\n" + SPACER);        
+//        sb.append(String.format(" Percentile Latencies 50: " + RESULT_FORMAT + "(ms) Latencies 95: "
+//                    + RESULT_FORMAT + "(ms) Latencies 99: " + RESULT_FORMAT + "(ms)", intervalPerLatency[0],intervalPerLatency[1], intervalPerLatency[2]));
+
+        sb.append("\n" + SPACER);
         sb.append("Since the benchmark began:");
         sb.append("\n" + SPACER + SPACER);
         sb.append(String.format("Completed %d txns at a rate of " + RESULT_FORMAT + " txns/s",
                                 totalTxnCount, totalTxnCount / (double)(pollIndex * results.getIntervalDuration()) * 1000d));
         sb.append(String.format(" with " + RESULT_FORMAT + " ms avg latency", totalLatency));
-        
+
         System.out.println();
         if (LOG.isDebugEnabled()) LOG.debug("Printing result information for poll index " + pollIndex);
         System.out.println(sb);
