b865c02bdad7e2505d3ca6072d0f193f37e09e7b
Anurag Khandelwal <anuragk@berkeley.edu>
2016-07-21 02:54:20 -0700

    Fixed field ids in SearchField

--- src/benchmarks/edu/berkeley/cs/benchmark/conviva/procedures/SearchField.java
+++ src/benchmarks/edu/berkeley/cs/benchmark/conviva/procedures/SearchField.java
@@ -6,11 +6,10 @@ import org.voltdb.VoltTable;
 
 public class SearchField extends VoltProcedure {
 
-  public final SQLStmt searchStmt5 = new SQLStmt("SELECT * FROM CONVIVATABLE WHERE FIELD5 = ?");
   public final SQLStmt searchStmt6 = new SQLStmt("SELECT * FROM CONVIVATABLE WHERE FIELD6 = ?");
   public final SQLStmt searchStmt7 = new SQLStmt("SELECT * FROM CONVIVATABLE WHERE FIELD7 = ?");
   public final SQLStmt searchStmt8 = new SQLStmt("SELECT * FROM CONVIVATABLE WHERE FIELD8 = ?");
-  public final SQLStmt searchStmt10 = new SQLStmt("SELECT * FROM CONVIVATABLE WHERE FIELD10 = ?");
+  public final SQLStmt searchStmt9 = new SQLStmt("SELECT * FROM CONVIVATABLE WHERE FIELD9 = ?");
   public final SQLStmt searchStmt11 = new SQLStmt("SELECT * FROM CONVIVATABLE WHERE FIELD11 = ?");
   public final SQLStmt searchStmt12 = new SQLStmt("SELECT * FROM CONVIVATABLE WHERE FIELD12 = ?");
   public final SQLStmt searchStmt13 = new SQLStmt("SELECT * FROM CONVIVATABLE WHERE FIELD13 = ?");
@@ -20,42 +19,40 @@ public class SearchField extends VoltProcedure {
   public final SQLStmt searchStmt17 = new SQLStmt("SELECT * FROM CONVIVATABLE WHERE FIELD17 = ?");
   public final SQLStmt searchStmt18 = new SQLStmt("SELECT * FROM CONVIVATABLE WHERE FIELD18 = ?");
   public final SQLStmt searchStmt19 = new SQLStmt("SELECT * FROM CONVIVATABLE WHERE FIELD19 = ?");
-  public final SQLStmt searchStmt21 = new SQLStmt("SELECT * FROM CONVIVATABLE WHERE FIELD21 = ?");
-  public final SQLStmt searchStmt24 = new SQLStmt("SELECT * FROM CONVIVATABLE WHERE FIELD24 = ?");
-  public final SQLStmt searchStmt27 = new SQLStmt("SELECT * FROM CONVIVATABLE WHERE FIELD27 = ?");
-  public final SQLStmt searchStmt31 = new SQLStmt("SELECT * FROM CONVIVATABLE WHERE FIELD31 = ?");
+  public final SQLStmt searchStmt20 = new SQLStmt("SELECT * FROM CONVIVATABLE WHERE FIELD20 = ?");
+  public final SQLStmt searchStmt22 = new SQLStmt("SELECT * FROM CONVIVATABLE WHERE FIELD22 = ?");
+  public final SQLStmt searchStmt25 = new SQLStmt("SELECT * FROM CONVIVATABLE WHERE FIELD25 = ?");
+  public final SQLStmt searchStmt28 = new SQLStmt("SELECT * FROM CONVIVATABLE WHERE FIELD28 = ?");
   public final SQLStmt searchStmt32 = new SQLStmt("SELECT * FROM CONVIVATABLE WHERE FIELD32 = ?");
-  public final SQLStmt searchStmt35 = new SQLStmt("SELECT * FROM CONVIVATABLE WHERE FIELD35 = ?");
+  public final SQLStmt searchStmt33 = new SQLStmt("SELECT * FROM CONVIVATABLE WHERE FIELD33 = ?");
   public final SQLStmt searchStmt36 = new SQLStmt("SELECT * FROM CONVIVATABLE WHERE FIELD36 = ?");
   public final SQLStmt searchStmt37 = new SQLStmt("SELECT * FROM CONVIVATABLE WHERE FIELD37 = ?");
   public final SQLStmt searchStmt38 = new SQLStmt("SELECT * FROM CONVIVATABLE WHERE FIELD38 = ?");
   public final SQLStmt searchStmt39 = new SQLStmt("SELECT * FROM CONVIVATABLE WHERE FIELD39 = ?");
   public final SQLStmt searchStmt40 = new SQLStmt("SELECT * FROM CONVIVATABLE WHERE FIELD40 = ?");
-  public final SQLStmt searchStmt52 = new SQLStmt("SELECT * FROM CONVIVATABLE WHERE FIELD52 = ?");
-  public final SQLStmt searchStmt55 = new SQLStmt("SELECT * FROM CONVIVATABLE WHERE FIELD55 = ?");
+  public final SQLStmt searchStmt41 = new SQLStmt("SELECT * FROM CONVIVATABLE WHERE FIELD41 = ?");
+  public final SQLStmt searchStmt53 = new SQLStmt("SELECT * FROM CONVIVATABLE WHERE FIELD53 = ?");
   public final SQLStmt searchStmt56 = new SQLStmt("SELECT * FROM CONVIVATABLE WHERE FIELD56 = ?");
   public final SQLStmt searchStmt57 = new SQLStmt("SELECT * FROM CONVIVATABLE WHERE FIELD57 = ?");
   public final SQLStmt searchStmt58 = new SQLStmt("SELECT * FROM CONVIVATABLE WHERE FIELD58 = ?");
   public final SQLStmt searchStmt59 = new SQLStmt("SELECT * FROM CONVIVATABLE WHERE FIELD59 = ?");
-  public final SQLStmt searchStmt61 = new SQLStmt("SELECT * FROM CONVIVATABLE WHERE FIELD61 = ?");
-  public final SQLStmt searchStmt64 = new SQLStmt("SELECT * FROM CONVIVATABLE WHERE FIELD64 = ?");
-  public final SQLStmt searchStmt67 = new SQLStmt("SELECT * FROM CONVIVATABLE WHERE FIELD67 = ?");
-  public final SQLStmt searchStmt73 = new SQLStmt("SELECT * FROM CONVIVATABLE WHERE FIELD73 = ?");
+  public final SQLStmt searchStmt60 = new SQLStmt("SELECT * FROM CONVIVATABLE WHERE FIELD60 = ?");
+  public final SQLStmt searchStmt62 = new SQLStmt("SELECT * FROM CONVIVATABLE WHERE FIELD62 = ?");
+  public final SQLStmt searchStmt65 = new SQLStmt("SELECT * FROM CONVIVATABLE WHERE FIELD65 = ?");
+  public final SQLStmt searchStmt68 = new SQLStmt("SELECT * FROM CONVIVATABLE WHERE FIELD68 = ?");
   public final SQLStmt searchStmt74 = new SQLStmt("SELECT * FROM CONVIVATABLE WHERE FIELD74 = ?");
-  public final SQLStmt searchStmt76 = new SQLStmt("SELECT * FROM CONVIVATABLE WHERE FIELD76 = ?");
-  public final SQLStmt searchStmt78 = new SQLStmt("SELECT * FROM CONVIVATABLE WHERE FIELD78 = ?");
-  public final SQLStmt searchStmt82 = new SQLStmt("SELECT * FROM CONVIVATABLE WHERE FIELD82 = ?");
-  public final SQLStmt searchStmt86 = new SQLStmt("SELECT * FROM CONVIVATABLE WHERE FIELD86 = ?");
+  public final SQLStmt searchStmt75 = new SQLStmt("SELECT * FROM CONVIVATABLE WHERE FIELD75 = ?");
+  public final SQLStmt searchStmt77 = new SQLStmt("SELECT * FROM CONVIVATABLE WHERE FIELD77 = ?");
+  public final SQLStmt searchStmt79 = new SQLStmt("SELECT * FROM CONVIVATABLE WHERE FIELD79 = ?");
+  public final SQLStmt searchStmt83 = new SQLStmt("SELECT * FROM CONVIVATABLE WHERE FIELD83 = ?");
   public final SQLStmt searchStmt87 = new SQLStmt("SELECT * FROM CONVIVATABLE WHERE FIELD87 = ?");
-  public final SQLStmt searchStmt92 = new SQLStmt("SELECT * FROM CONVIVATABLE WHERE FIELD92 = ?");
-  public final SQLStmt searchStmt102 = new SQLStmt("SELECT * FROM CONVIVATABLE WHERE FIELD102 = ?");
+  public final SQLStmt searchStmt88 = new SQLStmt("SELECT * FROM CONVIVATABLE WHERE FIELD88 = ?");
+  public final SQLStmt searchStmt93 = new SQLStmt("SELECT * FROM CONVIVATABLE WHERE FIELD93 = ?");
+  public final SQLStmt searchStmt103 = new SQLStmt("SELECT * FROM CONVIVATABLE WHERE FIELD103 = ?");
 
   public VoltTable[] run(int attrId, String value) {
 
     switch (attrId) {
-      case 5:
-        voltQueueSQL(searchStmt5, value);
-        break;
       case 6:
         voltQueueSQL(searchStmt6, value);
         break;
@@ -65,8 +62,8 @@ public class SearchField extends VoltProcedure {
       case 8:
         voltQueueSQL(searchStmt8, value);
         break;
-      case 10:
-        voltQueueSQL(searchStmt10, value);
+      case 9:
+        voltQueueSQL(searchStmt9, value);
         break;
       case 11:
         voltQueueSQL(searchStmt11, value);
@@ -95,23 +92,23 @@ public class SearchField extends VoltProcedure {
       case 19:
         voltQueueSQL(searchStmt19, value);
         break;
-      case 21:
-        voltQueueSQL(searchStmt21, value);
+      case 20:
+        voltQueueSQL(searchStmt20, value);
         break;
-      case 24:
-        voltQueueSQL(searchStmt24, value);
+      case 22:
+        voltQueueSQL(searchStmt22, value);
         break;
-      case 27:
-        voltQueueSQL(searchStmt27, value);
+      case 25:
+        voltQueueSQL(searchStmt25, value);
         break;
-      case 31:
-        voltQueueSQL(searchStmt31, value);
+      case 28:
+        voltQueueSQL(searchStmt28, value);
         break;
       case 32:
         voltQueueSQL(searchStmt32, value);
         break;
-      case 35:
-        voltQueueSQL(searchStmt35, value);
+      case 33:
+        voltQueueSQL(searchStmt33, value);
         break;
       case 36:
         voltQueueSQL(searchStmt36, value);
@@ -128,11 +125,11 @@ public class SearchField extends VoltProcedure {
       case 40:
         voltQueueSQL(searchStmt40, value);
         break;
-      case 52:
-        voltQueueSQL(searchStmt52, value);
+      case 41:
+        voltQueueSQL(searchStmt41, value);
         break;
-      case 55:
-        voltQueueSQL(searchStmt55, value);
+      case 53:
+        voltQueueSQL(searchStmt53, value);
         break;
       case 56:
         voltQueueSQL(searchStmt56, value);
@@ -146,41 +143,44 @@ public class SearchField extends VoltProcedure {
       case 59:
         voltQueueSQL(searchStmt59, value);
         break;
-      case 61:
-        voltQueueSQL(searchStmt61, value);
+      case 60:
+        voltQueueSQL(searchStmt60, value);
         break;
-      case 64:
-        voltQueueSQL(searchStmt64, value);
+      case 62:
+        voltQueueSQL(searchStmt62, value);
         break;
-      case 67:
-        voltQueueSQL(searchStmt67, value);
+      case 65:
+        voltQueueSQL(searchStmt65, value);
         break;
-      case 73:
-        voltQueueSQL(searchStmt73, value);
+      case 68:
+        voltQueueSQL(searchStmt68, value);
         break;
       case 74:
         voltQueueSQL(searchStmt74, value);
         break;
-      case 76:
-        voltQueueSQL(searchStmt76, value);
+      case 75:
+        voltQueueSQL(searchStmt75, value);
         break;
-      case 78:
-        voltQueueSQL(searchStmt78, value);
+      case 77:
+        voltQueueSQL(searchStmt77, value);
         break;
-      case 82:
-        voltQueueSQL(searchStmt82, value);
+      case 79:
+        voltQueueSQL(searchStmt79, value);
         break;
-      case 86:
-        voltQueueSQL(searchStmt86, value);
+      case 83:
+        voltQueueSQL(searchStmt83, value);
         break;
       case 87:
         voltQueueSQL(searchStmt87, value);
         break;
-      case 92:
-        voltQueueSQL(searchStmt92, value);
+      case 88:
+        voltQueueSQL(searchStmt88, value);
         break;
-      case 102:
-        voltQueueSQL(searchStmt102, value);
+      case 93:
+        voltQueueSQL(searchStmt93, value);
+        break;
+      case 103:
+        voltQueueSQL(searchStmt103, value);
         break;
       default:
         assert false : "Invalid attrId " + attrId;
